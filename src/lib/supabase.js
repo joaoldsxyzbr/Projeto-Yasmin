a72b60fa65e703415c5153e72d484da24e22a00b
João <contato@joaolds.xyz.br>
2026-08-16 18:24:16 -0300

    explicita politica de sessao supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const urlSupabase = 'https://vsqzwemlpnsffhfvtjai.supabase.co'
 const chavePublicavel = 'sb_publishable_MNAykD6Rj7qW5v9d2wHVpA_WE4hwqS6'
 
-export const supabase = createClient(urlSupabase, chavePublicavel)
+export const supabase = createClient(urlSupabase, chavePublicavel, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: false,
+  },
+})
