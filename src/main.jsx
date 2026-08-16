0946fce79c37fc12f2ff66ea28dcaa69b3022611
João <contato@joaolds.xyz.br>
2026-08-16 17:34:32 -0300

    bloqueia acesso publico ao app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { PortaoAcesso } from './componentes/PortaoAcesso'
 import './styles.css'
 import './autenticacao.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PortaoAcesso>
+      <App />
+    </PortaoAcesso>
   </StrictMode>,
 )
